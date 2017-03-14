646c4cf00d09224d3b4f8634cb05e86abb7fc3b5
Alexander Blair <alexander.blair@youcallmd.com>
2017-03-14 10:04:17 -0700

    Update for running pool on it's own.

--- init.js
+++ init.js
@@ -5,72 +5,59 @@ let argv = require('minimist')(process.argv.slice(2));
 let config = fs.readFileSync("./config.json");
 let coinConfig = fs.readFileSync("./coinConfig.json");
 let protobuf = require('protocol-buffers');
+let async = require('async');
 
 global.support = require("./lib/support.js")();
 global.config = JSON.parse(config);
-global.mysql = mysql.createPool(global.config.mysql);
 global.protos = protobuf(fs.readFileSync('./lib/data.proto'));
 let comms;
 let coinInc;
 
 // Config Table Layout
 // <module>.<item>
+if (global.config.hasOwnProperty('solo') && global.config.solo) {
+    require('./lib/pool.js');
+} else {
+    if (argv.module === 'api') {
+        global.mysql = mysql.createPool(global.config.mysql);
+        global.support.getSQLConfig(function (config_data) {
+                async.forEachOf(config_data, function (config_module, module_name, callback) {
+                    if (!global.config.hasOwnProperty(module_name)) {
+                        global.config[module_name] = {};
+                    }
+                    async.eachOfSeries(config_module, function (config_item, config_key, callback_int) {
+                        if (global.config[module_name].hasOwnProperty(config_key)) {
+                            return callback_int();
+                        }
+                        global.config[module_name][config_key] = config_item;
+                        return callback_int();
+                    }, function () {
+                        callback();
+                    });
+                }, function () {
+                    workerInit();
+                });
+            }
+        );
+    } else {
+        global.support.getConfig(global.config.configHost, workerInit);
+    }
+}
 
-global.mysql.query("SELECT * FROM config").then(function (rows) {
-    rows.forEach(function (row){
-        if (!global.config.hasOwnProperty(row.module)){
-            global.config[row.module] = {};
-        }
-        if (global.config[row.module].hasOwnProperty(row.item)){
-            return;
-        }
-        switch(row.item_type){
-            case 'int':
-                global.config[row.module][row.item] = parseInt(row.item_value);
-                break;
-            case 'bool':
-                global.config[row.module][row.item] = (row.item_value === "true");
-                break;
-            case 'string':
-                global.config[row.module][row.item] = row.item_value;
-                break;
-            case 'float':
-                global.config[row.module][row.item] = parseFloat(row.item_value);
-                break;
-        }
-    });
-}).then(function(){
+function workerInit() {
     global.config['coin'] = JSON.parse(coinConfig)[global.config.coin];
     coinInc = require(global.config.coin.funcFile);
     global.coinFuncs = new coinInc();
-    if (argv.module === 'pool'){
-        comms = require('./lib/remote_comms');
-    } else {
+    if (argv.module !== 'pool'){
         comms = require('./lib/local_comms');
+        global.database = new comms();
+        global.database.initEnv();
     }
-    global.database = new comms();
-    global.database.initEnv();
     global.coinFuncs.blockedAddresses.push(global.config.pool.address);
     global.coinFuncs.blockedAddresses.push(global.config.payout.feeAddress);
-    switch(argv.module){
+    switch (argv.module) {
         case 'pool':
-            global.config.ports = [];
-            global.mysql.query("SELECT * FROM port_config").then(function(rows){
-                rows.forEach(function(row){
-                    row.hidden = row.hidden === 1;
-                    row.ssl = row.ssl === 1;
-                    global.config.ports.push({
-                        port: row.poolPort,
-                        difficulty: row.difficulty,
-                        desc: row.portDesc,
-                        portType: row.portType,
-                        hidden: row.hidden,
-                        ssl: row.ssl
-                    });
-                });
-            }).then(function(){
-                require('./lib/pool.js');
-            });
+            require('./lib/pool.js');
             break;
         case 'blockManager':
             require('./lib/blockManager.js');
@@ -94,4 +81,4 @@ global.mysql.query("SELECT * FROM config").then(function (rows) {
             console.error("Invalid module provided.  Please provide a valid module");
             process.exit(1);
     }
-});
+}
